fd2fd50940b180c24a57f8272f881631c84fc5ee
laptise <laptise@live.jp>
2022-10-17 22:47:59 +0900

    change

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -8,6 +8,11 @@ import { CommenteeModule } from './commentee/commentee.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
+      host: 'localhost',
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     }),
     CommenteeModule,
   ],
